2322a5e0ce73a59fbba143bb3be4fc8b7383f1d5
slayerstokely <stokely@slayers.ru>
2025-07-17 21:14:20 +0300

    added dockerfile

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import { router } from "./routes.js";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Running on ${process.env.PORT || 3000}`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
 
 app.use("/", router);
